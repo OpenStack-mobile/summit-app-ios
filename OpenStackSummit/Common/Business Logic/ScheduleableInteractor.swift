943858586483977c9ca57be77dba169cd41f6182
Claudio Redi <claudio@tipit.net>
2015-11-10 16:51:16 -0300

    [claudioredi] - moved common logic to handle event scheduling to a common place

---
+++ OpenStackSummit/Common/Business Logic/ScheduleableInteractor.swift
@@ -0,0 +1,48 @@
+//
+//  ISchedueableInteractor.swift
+//  OpenStackSummit
+//
+//  Created by Claudio on 11/10/15.
+//  Copyright © 2015 OpenStack. All rights reserved.
+//
+
+import UIKit
+
+@objc
+public protocol IScheduleableInteractor {
+    func addEventToLoggedInMemberSchedule(eventId: Int, completionBlock: (NSError?) -> Void)
+    func removeEventFromLoggedInMemberSchedule(eventId: Int, completionBlock: (NSError?) -> Void)
+    func isEventScheduledByLoggedMember(eventId: Int) -> Bool
+}
+
+public class ScheduleableInteractor: NSObject, IScheduleableInteractor {
+    var summitAttendeeDataStore: ISummitAttendeeDataStore!
+    var securityManager: SecurityManager!
+    var eventDataStore: IEventDataStore!
+
+    public func addEventToLoggedInMemberSchedule(eventId: Int, completionBlock: (NSError?) -> Void) {
+        let loggedInMember = securityManager.getCurrentMember()
+        let event = eventDataStore.getByIdLocal(eventId)
+        
+        summitAttendeeDataStore.addEventToMemberShedule(loggedInMember!.attendeeRole!, event: event!) {(attendee, error) in
+            completionBlock(error)
+        }
+    }
+    
+    public func removeEventFromLoggedInMemberSchedule(eventId: Int, completionBlock: (NSError?) -> Void) {
+        let loggedInMember = securityManager.getCurrentMember()
+        let event = eventDataStore.getByIdLocal(eventId)
+        
+        summitAttendeeDataStore.removeEventFromMemberShedule(loggedInMember!.attendeeRole!, event: event!) {(attendee, error) in
+            completionBlock(error)
+        }
+    }
+    
+    public func isEventScheduledByLoggedMember(eventId: Int) -> Bool {
+        guard let loggedInMember = securityManager.getCurrentMember() else {
+            return false
+        }
+        
+        return loggedInMember.attendeeRole!.scheduledEvents.filter("id = \(eventId)").count > 0
+    }
+}
