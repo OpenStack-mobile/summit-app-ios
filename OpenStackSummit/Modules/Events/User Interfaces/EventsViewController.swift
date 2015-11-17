df3dce5d8571ea516dafc7b4d6f002c770a4b05e
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2015-11-17 11:07:24 -0300

    [gcutrini] - Added show filter functionality to events module.

--- OpenStackSummit/Modules/Events/User Interfaces/EventsViewController.swift
+++ OpenStackSummit/Modules/Events/User Interfaces/EventsViewController.swift
@@ -14,10 +14,31 @@ class EventsViewController: RevealTabStripViewController {
     
     var generalScheduleViewController: GeneralScheduleViewController!
     var trackListViewController: TrackListViewController!
+
+    var internalPresenter: IEventsPresenter!
+    
+    var presenter: IEventsPresenter! {
+        get {
+            return internalPresenter as IEventsPresenter
+        }
+        set {
+            internalPresenter = newValue
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if (filterButton != nil) {
+            filterButton.target = self
+            filterButton.action = Selector("showFilters:")
+        }
+        
         buttonBarView.registerNib(UINib(nibName: "ButtonCell", bundle: nil), forCellWithReuseIdentifier: "Cell")
+    }
+    
+    func showFilters(sender: UIBarButtonItem) {
+        presenter.showFilters()
     }
     
     override func childViewControllersForPagerTabStripViewController(pagerTabStripViewController: XLPagerTabStripViewController) -> [AnyObject] {
