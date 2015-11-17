6762cc8ebc54b763b540c97e11d6a57348296e73
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2015-11-17 11:07:24 -0300

    [gcutrini] - Added show filter functionality to events module.

--- OpenStackSummit/Assembly/EventsAssembly.swift
+++ OpenStackSummit/Assembly/EventsAssembly.swift
@@ -12,14 +12,33 @@ import Typhoon
 
 class EventsAssembly: TyphoonAssembly {
     var generalScheduleAssembly: GeneralScheduleAssembly!
+    var generalScheduleFilterAssembly: GeneralScheduleFilterAssembly!
     var trackListAssembly: TrackListAssembly!
-
+    
+    dynamic func eventsWireframe() -> AnyObject {
+        return TyphoonDefinition.withClass(EventsWireframe.self) {
+            (definition) in
+            
+            definition.injectProperty("eventsViewController", with: self.eventsViewController())
+            definition.injectProperty("generalScheduleFilterWireframe", with: self.generalScheduleFilterAssembly.generalScheduleFilterWireframe())
+        }
+    }
+    
+    dynamic func eventsPresenter() -> AnyObject {
+        return TyphoonDefinition.withClass(EventsPresenter.self) {
+            (definition) in
+            
+            definition.injectProperty("wireframe", with: self.eventsWireframe())
+        }
+    }
+    
     dynamic func eventsViewController() -> AnyObject {
         return TyphoonDefinition.withClass(EventsViewController.self) {
             (definition) in
             
             definition.injectProperty("generalScheduleViewController", with: self.generalScheduleAssembly.generalScheduleViewController())
             definition.injectProperty("trackListViewController", with: self.trackListAssembly.trackListViewController())
+            definition.injectProperty("presenter", with: self.eventsPresenter())
         }
     }
 }
