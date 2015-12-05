f4a2d3be6494aa805828638edc3ad191781bd900
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2015-12-05 19:14:22 -0300

    [gcutrini] - #9699 Implemented feedback tableview cell's UI.

--- OpenStackSummit/Common/DTOs/Assemblers/FeedbackDTOAssembler.swift
+++ OpenStackSummit/Common/DTOs/Assemblers/FeedbackDTOAssembler.swift
@@ -21,10 +21,72 @@ public class FeedbackDTOAssembler: NSObject, IFeedbackDTOAssembler {
         feedbackDTO.review = feedback.review
         feedbackDTO.eventId = feedback.event.id
         feedbackDTO.eventName = feedback.event.name
-        feedbackDTO.date = getDate(feedback)
+        feedbackDTO.date = timeAgoSinceDate(feedback.date, numericDates: false)
         feedbackDTO.owner = feedback.owner.firstName + " " + feedback.owner.lastName
         
         return feedbackDTO
+    }
+    
+    func timeAgoSinceDate(date:NSDate, numericDates:Bool) -> String {
+        let calendar = NSCalendar.currentCalendar()
+        let now = NSDate()
+        let earliest = now.earlierDate(date)
+        let latest = (earliest == now) ? date : now
+        let components:NSDateComponents = calendar.components([NSCalendarUnit.Minute , NSCalendarUnit.Hour , NSCalendarUnit.Day , NSCalendarUnit.WeekOfYear , NSCalendarUnit.Month , NSCalendarUnit.Year , NSCalendarUnit.Second], fromDate: earliest, toDate: latest, options: NSCalendarOptions())
+        
+        if (components.year >= 2) {
+            return "\(components.year) years ago"
+        } else if (components.year >= 1){
+            if (numericDates){
+                return "1 year ago"
+            } else {
+                return "Last year"
+            }
+        } else if (components.month >= 2) {
+            return "\(components.month) months ago"
+        } else if (components.month >= 1){
+            if (numericDates){
+                return "1 month ago"
+            } else {
+                return "Last month"
+            }
+        } else if (components.weekOfYear >= 2) {
+            return "\(components.weekOfYear) weeks ago"
+        } else if (components.weekOfYear >= 1){
+            if (numericDates){
+                return "1 week ago"
+            } else {
+                return "Last week"
+            }
+        } else if (components.day >= 2) {
+            return "\(components.day) days ago"
+        } else if (components.day >= 1){
+            if (numericDates){
+                return "1 day ago"
+            } else {
+                return "Yesterday"
+            }
+        } else if (components.hour >= 2) {
+            return "\(components.hour) hours ago"
+        } else if (components.hour >= 1){
+            if (numericDates){
+                return "1 hour ago"
+            } else {
+                return "An hour ago"
+            }
+        } else if (components.minute >= 2) {
+            return "\(components.minute) minutes ago"
+        } else if (components.minute >= 1){
+            if (numericDates){
+                return "1 minute ago"
+            } else {
+                return "A minute ago"
+            }
+        } else if (components.second >= 3) {
+            return "\(components.second) seconds ago"
+        } else {
+            return "Just now"
+        }
     }
     
     public func getDate(feedback: Feedback) -> String{
