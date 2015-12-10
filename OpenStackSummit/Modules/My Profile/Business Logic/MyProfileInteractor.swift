685fafa5e62dccfd3dcb77dd154c38149046c1fa
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2015-12-10 09:07:10 -0300

    [gcutrini] - Set speaker profile section title to speaker name.

--- OpenStackSummit/Modules/My Profile/Business Logic/MyProfileInteractor.swift
+++ OpenStackSummit/Modules/My Profile/Business Logic/MyProfileInteractor.swift
@@ -10,13 +10,31 @@ import Foundation
 
 @objc
 public protocol IMyProfileInteractor {
+    func getSpeakerProfile(speakerId: Int, completionBlock : (PresentationSpeakerDTO?, NSError?) -> Void)
     func getCurrentMember() -> MemberDTO?
 }
 
 public class MyProfileInteractor: NSObject, IMyProfileInteractor {
     
+    var presentationSpeakerRemoteDataStore: IPresentationSpeakerRemoteDataStore!
+    var summitAttendeeRemoteDataStore: ISummitAttendeeRemoteDataStore!
+    var summitAttendeeDTOAssembler: ISummitAttendeeDTOAssembler!
+    var presentationSpeakerDTOAssembler: IPresentationSpeakerDTOAssembler!
     var memberDTOAssembler: IMemberDTOAssembler!
     var securityManager: SecurityManager!
+    
+    public func getSpeakerProfile(speakerId: Int, completionBlock : (PresentationSpeakerDTO?, NSError?) -> Void) {
+        presentationSpeakerRemoteDataStore.getById(speakerId) { speaker, error in
+            
+            if (error != nil) {
+                completionBlock(nil, error)
+                return
+            }
+            
+            let speakerDTO = self.presentationSpeakerDTOAssembler.createDTO(speaker!)
+            completionBlock(speakerDTO, error)
+        }
+    }
     
     public func getCurrentMember() -> MemberDTO? {
         var memberDTO: MemberDTO?
