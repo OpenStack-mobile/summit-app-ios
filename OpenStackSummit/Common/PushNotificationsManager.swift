d1b80d68c0544c1838b9f2fe9a522554943a3727
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2016-01-06 11:54:08 -0500

    [gcutrini] - Added summit check in notification setup.

--- OpenStackSummit/Common/PushNotificationsManager.swift
+++ OpenStackSummit/Common/PushNotificationsManager.swift
@@ -20,32 +20,34 @@ public class PushNotificationsManager: NSObject, IPushNotificationsManager {
     var summitDataStore: ISummitDataStore!
     
     public func subscribeToPushChannelsUsingContext(completionBlock: (succeeded: Bool, error: NSError?) -> Void) {
-        let summit = summitDataStore.getActiveLocal()
         var channels = [String]()
         
-        channels.append("su_\(summit!.id)")
-        
-        if let member = self.securityManager.getCurrentMember() {
-            channels.append("me_\(member.id)")
-            channels.append("attendees")
-            if member.speakerRole != nil {
-                channels.append("speakers")
+        if let summit = summitDataStore.getActiveLocal() {
+            channels.append("su_\(summit.id)")
+            
+            if let member = self.securityManager.getCurrentMember() {
+                channels.append("me_\(member.id)")
+                channels.append("attendees")
+                if member.speakerRole != nil {
+                    channels.append("speakers")
+                }
+                
+                PFInstallation.currentInstallation().channels = channels
+                PFInstallation.currentInstallation().saveEventually(completionBlock)
             }
             
             PFInstallation.currentInstallation().channels = channels
             PFInstallation.currentInstallation().saveEventually(completionBlock)
         }
-        
-        PFInstallation.currentInstallation().channels = channels
-        PFInstallation.currentInstallation().saveEventually(completionBlock)
-        
     }
     
     public func unsubscribeFromPushChannels(completionBlock: (succeeded: Bool, error: NSError?) -> Void) {
         var channels = [String]()
-        let summit = summitDataStore.getActiveLocal()
-        channels.append("su_\(summit!.id)")
-        PFInstallation.currentInstallation().channels = channels
-        PFInstallation.currentInstallation().saveEventually(completionBlock)
+        
+        if let summit = summitDataStore.getActiveLocal() {
+            channels.append("su_\(summit.id)")
+            PFInstallation.currentInstallation().channels = channels
+            PFInstallation.currentInstallation().saveEventually(completionBlock)
+        }
     }
 }
