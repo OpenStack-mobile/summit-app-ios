ee8ae4a6eed567a276b21f0d56da71ae0e58e095
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2016-03-03 10:58:38 -0300

    [gcutrini] - #10491 Added missing migration.

--- OpenStackSummit/Common/DataAccess/SchemaMigration.swift
+++ OpenStackSummit/Common/DataAccess/SchemaMigration.swift
@@ -15,7 +15,7 @@ protocol ISchemaMigration {
 }
 
 public class SchemaMigration: NSObject, ISchemaMigration {
-    let schemaVersion:UInt64 = 3
+    let schemaVersion:UInt64 = 4
     
     func getSchemaVersion() -> UInt64 {
         return schemaVersion
@@ -45,6 +45,11 @@ public class SchemaMigration: NSObject, ISchemaMigration {
                             track["trackGroup"] = newObject
                         }
                     }
+                }
+            }
+            if (oldSchemaVersion < 4) {
+                migration.enumerate(DataUpdate.className()) { oldObject, newObject in
+                    newObject!["operation"] = DataOperation.NoOp.rawValue
                 }
             }
         }
