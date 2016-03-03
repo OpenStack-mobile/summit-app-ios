58f9826eedf7b042ae621c9628c67b476ec65685
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2016-03-03 17:05:41 -0300

    [gcutrini] - #10568 Ignores speakers with empty name on speakers list and event detail.

--- OpenStackSummit/Common/Entities/Presentation.swift
+++ OpenStackSummit/Common/Entities/Presentation.swift
@@ -10,10 +10,10 @@ import Foundation
 import RealmSwift
 
 public class Presentation: BaseEntity {
-    
+
     public dynamic var level = ""
     public dynamic var track: Track!
-    public dynamic var  moderator: PresentationSpeaker?
+    public dynamic var moderator: PresentationSpeaker?
     public let speakers = List<PresentationSpeaker>()
     public var event: SummitEvent {
         return linkingObjects(SummitEvent.self, forProperty: "presentation").first!
