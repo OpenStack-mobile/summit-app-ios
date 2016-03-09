2a089f0b869056b55c7c955ffb6fb70449e6aa61
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2016-03-09 18:46:39 -0300

    [gcutrini] - Added missing personal schedule presenter method implementation.

--- OpenStackSummit/Modules/Personal Schedule/Business Logic/PersonalScheduleInteractor.swift
+++ OpenStackSummit/Modules/Personal Schedule/Business Logic/PersonalScheduleInteractor.swift
@@ -10,10 +10,27 @@ import UIKit
 
 @objc
 public protocol IPersonalScheduleInteractor: IScheduleInteractor {
+    func getLoggedInMemberScheduledEventsDatesFrom(startDate: NSDate, to endDate: NSDate) -> [NSDate]
     func getLoggedInMemberScheduledEventsFrom(startDate: NSDate, to endDate: NSDate) -> [ScheduleItemDTO]
 }
 
 public class PersonalScheduleInteractor: ScheduleInteractor, IPersonalScheduleInteractor {
+    
+    public func getLoggedInMemberScheduledEventsDatesFrom(startDate: NSDate, to endDate: NSDate) -> [NSDate] {
+        let currentMember = securityManager.getCurrentMember()
+        let events = currentMember?.attendeeRole?.scheduledEvents.filter("start >= %@ and end <= %@", startDate, endDate).sorted("start")
+        
+        var activeDates: [NSDate] = []
+        for event in events! {
+            let timeZone = NSTimeZone(name: event.summit.timeZone)!
+            let startDate = event.start.mt_dateSecondsAfter(timeZone.secondsFromGMT).mt_startOfCurrentDay()
+            if !activeDates.contains(startDate) {
+                activeDates.append(startDate)
+            }
+            
+        }
+        return activeDates
+    }
     
     public func getLoggedInMemberScheduledEventsFrom(startDate: NSDate, to endDate: NSDate) -> [ScheduleItemDTO] {
         let currentMember = securityManager.getCurrentMember()
