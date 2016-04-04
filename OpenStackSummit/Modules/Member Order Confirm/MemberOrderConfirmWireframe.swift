a1cb1a614de6fb4cc64be2e24fc659c1ac0a1e70
Claudio Redi <claudio@tipit.net>
2016-04-04 09:26:02 -0300

    [claudioredi] - #10786 * WiP for order confirmation

---
+++ OpenStackSummit/Modules/Member Order Confirm/MemberOrderConfirmWireframe.swift
@@ -0,0 +1,13 @@
+//
+//  MemberOrderConfirmWireframe.swift
+//  OpenStackSummit
+//
+//  Created by Claudio on 4/3/16.
+//  Copyright © 2016 OpenStack. All rights reserved.
+//
+
+import UIKit
+
+class MemberOrderConfirmWireframe: NSObject {
+
+}
