a4d2f2e614d727328ff98b7696881aa0379e6a92
Claudio Redi <claudio@tipit.net>
2016-04-06 18:46:04 -0300

    [claudioredi] - #10786 * WiP for order confirmation

--- OpenStackSummit/Modules/Member Order Confirm/User Interface/MemberOrderConfirmViewController.swift
+++ OpenStackSummit/Modules/Member Order Confirm/User Interface/MemberOrderConfirmViewController.swift
@@ -13,19 +13,24 @@ import SwiftSpinner
 protocol IMemberOrderConfirmViewController: IMessageEnabledViewController{
     func showActivityIndicator()
     func hideActivityIndicator()
+    func setSummitAttendees(attendees: [NamedDTO])
+    func showAttendeesSelector(show: Bool)
 }
 
-class MemberOrderConfirmViewController: BaseViewController, IMemberOrderConfirmViewController, UITextFieldDelegate {
+class MemberOrderConfirmViewController: RevealViewController, IMemberOrderConfirmViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
     @IBOutlet weak var orderNumberText: UITextField!
     @IBOutlet weak var personPicker: UIPickerView!
+    @IBOutlet weak var multipleAttendeesMatchingLabel: UILabel!
     var presenter : IMemberOrderConfirmPresenter!
+    var attendees: [NamedDTO]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         presenter.viewLoad()
         orderNumberText.delegate = self
+        navigationController?.navigationBar.topItem?.title = "MY PROFILE"
     }
     
     func showActivityIndicator() {
@@ -42,5 +47,37 @@ class MemberOrderConfirmViewController: BaseViewController, IMemberOrderConfirmV
             presenter.orderConfirm(orderNumberText.text!)            
         }
         return true
+    }
+    
+    func setSummitAttendees(attendees: [NamedDTO]) {
+        self.attendees = [ NamedDTO() ]
+        self.attendees.appendContentsOf(attendees)
+        if attendees.count > 0 {
+            multipleAttendeesMatchingLabel.hidden = false
+            personPicker.hidden = false
+            personPicker.delegate = self
+            personPicker.dataSource = self
+        }
+    }
+    
+    func showAttendeesSelector(show: Bool) {
+        personPicker.hidden = show
+        multipleAttendeesMatchingLabel.hidden = show
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return attendees.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return attendees[row].name
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        presenter.selectAttendeeFromOrderList(row - 1)
     }
 }
