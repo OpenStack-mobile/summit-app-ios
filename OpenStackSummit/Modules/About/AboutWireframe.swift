4cc361657de23ab5136c7e9e5473f525e17ac6e4
Claudio Redi <claudio@tipit.net>
2016-04-07 00:25:05 -0300

    [claudioredi] - #10847 * Fixed bug processing feedback

---
+++ OpenStackSummit/Modules/About/AboutWireframe.swift
@@ -0,0 +1,35 @@
+//
+//  AboutWireframe.swift
+//  OpenStackSummit
+//
+//  Created by Claudio on 4/6/16.
+//  Copyright © 2016 OpenStack. All rights reserved.
+//
+
+import UIKit
+
+class AboutWireframe: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
