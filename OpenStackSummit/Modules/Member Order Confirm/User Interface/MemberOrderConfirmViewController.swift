d2407febac69cd85ae35e379cbcf6c3dd24f0f19
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2016-04-10 09:08:36 -0400

    [gcutrini] - #10786 Fixed UI issues. Added scroll view.

--- OpenStackSummit/Modules/Member Order Confirm/User Interface/MemberOrderConfirmViewController.swift
+++ OpenStackSummit/Modules/Member Order Confirm/User Interface/MemberOrderConfirmViewController.swift
@@ -19,9 +19,11 @@ protocol IMemberOrderConfirmViewController: IMessageEnabledViewController{
 
 class MemberOrderConfirmViewController: RevealViewController, IMemberOrderConfirmViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var orderNumberText: UITextField!
     @IBOutlet weak var personPicker: UIPickerView!
     @IBOutlet weak var multipleAttendeesMatchingLabel: UILabel!
+    
     var presenter : IMemberOrderConfirmPresenter!
     var attendees: [NamedDTO]!
     
@@ -41,13 +43,46 @@ class MemberOrderConfirmViewController: RevealViewController, IMemberOrderConfir
         navigationController?.navigationBar.topItem?.title = "MY PROFILE"
     }
     
-    func menuButtonPressed(sender: UIBarButtonItem) {
-        orderNumberText.resignFirstResponder()
-        revealViewController().revealToggle(sender)
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        registerKeyboardNotifications()
+        orderNumberText.text = ""
     }
     
-    func resignOnTap(sender: AnyObject) {
-        orderNumberText.resignFirstResponder()
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        unregisterKeyboardNotifications()
+    }
+    
+    func registerKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unregisterKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        let userInfo: NSDictionary = notification.userInfo!
+        let keyboardSize = userInfo.objectForKey(UIKeyboardFrameBeginUserInfoKey)!.CGRectValue.size
+        let contentInsets = UIEdgeInsetsMake(0, 0, keyboardSize.height, 0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        
+        var viewRect = view.frame
+        viewRect.size.height -= keyboardSize.height
+        if CGRectContainsPoint(viewRect, orderNumberText.frame.origin) {
+            let scrollPoint = CGPointMake(0, orderNumberText.frame.origin.y - keyboardSize.height)
+            scrollView.setContentOffset(scrollPoint, animated: true)
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        scrollView.contentInset = UIEdgeInsetsZero
+        scrollView.scrollIndicatorInsets = UIEdgeInsetsZero
+        let scrollPoint = CGPointMake(0, 0)
+        scrollView.setContentOffset(scrollPoint, animated: true)
     }
     
     func showActivityIndicator() {
@@ -56,6 +91,15 @@ class MemberOrderConfirmViewController: RevealViewController, IMemberOrderConfir
     
     func hideActivityIndicator() {
         SwiftSpinner.hide()
+    }
+    
+    func menuButtonPressed(sender: UIBarButtonItem) {
+        orderNumberText.resignFirstResponder()
+        revealViewController().revealToggle(sender)
+    }
+    
+    func resignOnTap(sender: AnyObject) {
+        orderNumberText.resignFirstResponder()
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
@@ -78,8 +122,8 @@ class MemberOrderConfirmViewController: RevealViewController, IMemberOrderConfir
     }
     
     func showAttendeesSelector(show: Bool) {
-        personPicker.hidden = show
-        multipleAttendeesMatchingLabel.hidden = show
+        personPicker.hidden = !show
+        multipleAttendeesMatchingLabel.hidden = !show
     }
     
     func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
@@ -95,6 +139,8 @@ class MemberOrderConfirmViewController: RevealViewController, IMemberOrderConfir
     }
     
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        presenter.selectAttendeeFromOrderList(row - 1)
+        if row > 0 {
+            presenter.selectAttendeeFromOrderList(row - 1)
+        }
     }
 }
