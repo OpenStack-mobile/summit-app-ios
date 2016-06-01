86982fdde4cf19dd7d8fbddd3288a4f72e3df9c1
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-01 13:09:30 -0500

    added TrackGroup

---
+++ CoreSummit/TrackGroup.swift
@@ -0,0 +1,20 @@
+//
+//  TrackGroup.swift
+//  OpenStackSummit
+//
+//  Created by Alsey Coleman Miller on 6/1/16.
+//  Copyright © 2016 OpenStack. All rights reserved.
+//
+
+public struct TrackGroup: Named {
+    
+    public let identifier: Identifier
+    
+    public var name: String
+    
+    public var color: String
+    
+    public var descriptionText: String
+    
+    public var tracks: Int
+}
---
+++ CoreSummit/TrackGroupJSON.swift
@@ -0,0 +1,38 @@
+//
+//  TrackGroupJSON.swift
+//  OpenStackSummit
+//
+//  Created by Alsey Coleman Miller on 6/1/16.
+//  Copyright © 2016 OpenStack. All rights reserved.
+//
+
+import SwiftFoundation
+
+public extension TrackGroup {
+    
+    enum JSONKey: String {
+        
+        case id, name, color, description, tracks
+    }
+}
+
+extension TrackGroup: JSONDecodable {
+    
+    public init?(JSONValue: JSON.Value) {
+        
+        guard let JSONObject = JSONValue.objectValue,
+            let identifier = JSONObject[JSONKey.id.rawValue]?.rawValue as? Int,
+            let name = JSONObject[JSONKey.name.rawValue]?.rawValue as? String,
+            let color = JSONObject[JSONKey.color.rawValue]?.rawValue as? String,
+            let description = JSONObject[JSONKey.description.rawValue]?.rawValue as? String,
+            let tracksJSONArray = JSONObject[JSONKey.tracks.rawValue]?.arrayValue,
+            let tracks = Int.fromJSON(tracksJSONArray)?.first
+            else { return nil }
+        
+        self.identifier = identifier
+        self.name = name
+        self.color = color
+        self.descriptionText = description
+        self.tracks = tracks
+    }
+}
