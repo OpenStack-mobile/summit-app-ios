e3216762cd65fdbfe7f265ee8cd9482bbddb0718
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-01 12:40:55 -0500

    added PresentationSpeaker

--- CoreSummit/PresentationSpeaker.swift
+++ CoreSummit/PresentationSpeaker.swift
@@ -6,7 +6,25 @@
 //  Copyright © 2016 OpenStack. All rights reserved.
 //
 
-public struct PresentationSpeaker: PersonProtocol {
+public struct PresentationSpeaker: Person {
     
+    public let identifier: Identifier
     
+    public var firstName: String
+    
+    public var lastName: String
+    
+    public var title: String
+    
+    public var pictureURL: String
+        
+    public var email: String
+    
+    public var twitter: String
+    
+    public var irc: String
+    
+    public var biography: String
+    
+    public var memberIdentifier: Identifier
 }
