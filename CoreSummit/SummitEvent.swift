c1a913fc0ced8189034277ac11fcb4ce34e2876a
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-01 17:42:55 -0500

    working on Summit Event JSON

--- CoreSummit/SummitEvent.swift
+++ CoreSummit/SummitEvent.swift
@@ -6,11 +6,21 @@
 //  Copyright © 2016 OpenStack. All rights reserved.
 //
 
+import struct SwiftFoundation.Date
+
 public struct SummitEvent: Named {
     
     public let identifier: Identifier
     
     public var name: String
     
+    public var descriptionText: String
     
+    public var start: Date
+    
+    public var end: Date
+    
+    public var allowFeedback: Bool
+    
+    public var averageFeedback: Double
 }
--- CoreSummit/SummitEventJSON.swift
+++ CoreSummit/SummitEventJSON.swift
@@ -6,4 +6,26 @@
 //  Copyright © 2016 OpenStack. All rights reserved.
 //
 
-import Foundation
+import SwiftFoundation
+
+public extension SummitEvent {
+    
+    enum JSONKey: String {
+        
+        case id, name, start_date, end_date, type_id, summit_types, sponsors, speakers, location_id, tags, track_id
+    }
+}
+
+extension SummitEvent: JSONDecodable {
+    
+    public init?(JSONValue: JSON.Value) {
+        
+        guard let JSONObject = JSONValue.objectValue,
+            let identifier = JSONObject[JSONKey.id.rawValue]?.rawValue as? Int,
+            let name = JSONObject[JSONKey.name.rawValue]?.rawValue as? String,
+            let startDate = JSONObject[JSONKey.start_date.rawValue]?.rawValue as? Int,
+            let endDate = JSONObject[JSONKey.end_date.rawValue]?.rawValue as? Int,
+            let eventType = JSONObject[JSONKey.type_id.rawValue]
+            else { return nil }
+    }
+}
