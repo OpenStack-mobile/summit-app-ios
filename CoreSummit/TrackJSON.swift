b7318c9b9c02ef93deb1964b1d08ac85b5dcef82
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-01 12:50:40 -0500

    added Track

---
+++ CoreSummit/TrackJSON.swift
@@ -0,0 +1,34 @@
+//
+//  TrackJSON.swift
+//  OpenStackSummit
+//
+//  Created by Alsey Coleman Miller on 6/1/16.
+//  Copyright © 2016 OpenStack. All rights reserved.
+//
+
+import SwiftFoundation
+
+public extension Track {
+    
+    enum JSONKey: String {
+        
+        case id, name, track_groups
+    }
+}
+
+extension Track: JSONDecodable {
+    
+    public init?(JSONValue: JSON.Value) {
+        
+        guard let JSONObject = JSONValue.objectValue,
+            let identifier = JSONObject[JSONKey.id.rawValue]?.rawValue as? Int,
+            let name = JSONObject[JSONKey.name.rawValue]?.rawValue as? String,
+            let trackGroupsJSONArray = JSONObject[JSONKey.track_groups.rawValue]?.arrayValue,
+            let trackGroups = Int.fromJSON(trackGroupsJSONArray)?.first
+            else { return nil }
+        
+        self.identifier = identifier
+        self.name = name
+        self.groups = trackGroups
+    }
+}
