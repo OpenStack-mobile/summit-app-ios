706d03eb246f1aeabdd972aadbea3529d8949d62
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-02 11:49:41 -0500

    successfully implemented Summit JSON Decoding

--- OpenStackSummit/Common/Entities/VenueRoom.swift
+++ OpenStackSummit/Common/Entities/VenueRoom.swift
@@ -13,7 +13,8 @@ public class VenueRoom: Location {
 
     public dynamic var capacity = 0
     public dynamic var venue: Venue!
-    public var events: [SummitEvent] {
-        return linkingObjects(SummitEvent.self, forProperty: "venueRoom")
+    
+    public var events: [SummitEvent] { return []
+        //return (self as RealmSwift.Object).linkingObjects(SummitEvent.self, forProperty: "venueRoom")
     }
 }
