0a7c8171edc4cdc8f6646c0fce3632866450a0e2
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-03 22:00:37 -0500

    spacing fix

--- CoreSummit/RealmTag.swift
+++ CoreSummit/RealmTag.swift
@@ -27,7 +27,7 @@ extension Tag: RealmEncodable {
         
         let realmEntity = RealmType.cached(identifier, realm: realm)
         
-                realmEntity.name = name
+        realmEntity.name = name
         
         return realmEntity
     }
