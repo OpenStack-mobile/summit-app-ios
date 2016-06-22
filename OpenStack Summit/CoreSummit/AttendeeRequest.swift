d1f73811f15930515ba30b261b4d9b4a01113a3e
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-06-22 16:18:49 -0500

    Working on SummitAttendee request

--- OpenStack Summit/CoreSummit/AttendeeRequest.swift
+++ OpenStack Summit/CoreSummit/AttendeeRequest.swift
@@ -12,11 +12,22 @@ import AeroGearOAuth2
 
 public extension Store {
     
-    func attendee(identifier: Int, completion: ErrorValue<SummitAttendee> -> ()) {
+    func attendee(identifier: Identifier, summit summitIdentifier: Identifier? = nil, completion: ErrorValue<SummitAttendee> -> ()) {
         
         let http = self.createHTTP(.ServiceAccount)
         
-        let URI = "/api/v1/summits/current/attendees/" + "\(identifier)"
+        let summitID: String
+        
+        if let identifier = summitIdentifier {
+            
+            summitID = "\(identifier)"
+            
+        } else {
+            
+            summitID = "current"
+        }
+        
+        let URI = "/api/v1/summits/" +  summitID + "/attendees/" + "\(identifier)"
         
         let URL = Constants.Urls.ResourceServerBaseUrl + URI
         
