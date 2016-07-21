61e36a138e5ef40de2225e1ee65e7b545dd2b630
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-07-21 11:23:54 -0500

    Added VenueListViewController

---
+++ OpenStack Summit/OpenStack Summit/InternalVenueListTableViewCell.swift
@@ -0,0 +1,57 @@
+//
+//  InternalVenueListTableViewCell.swift
+//  OpenStackSummit
+//
+//  Created by Gabriel Horacio Cutrini on 3/12/16.
+//  Copyright © 2016 OpenStack. All rights reserved.
+//
+
+import UIKit
+
+final class InternalVenueListTableViewCell: UITableViewCell, VenueListTableViewCellProtocol {
+    
+    // MARK: - IB Outlets
+    
+    @IBOutlet private weak var nameLabel: UILabel!
+    @IBOutlet private weak var addressLabel: UILabel!
+    @IBOutlet private weak var backgroundImageView: UIImageView!
+    
+    // MARK: - Accessors
+    
+    var name: String {
+        
+        get { return nameLabel.text ?? "" }
+        set { nameLabel.text = newValue }
+    }
+    
+    var address: String {
+        get { return addressLabel.text ?? "" }
+        set { addressLabel.text = newValue }
+    }
+    
+    var backgroundImageURL: String = "" {
+        didSet {
+            if backgroundImageURL.isEmpty {
+                backgroundImageView.image = nil
+            }
+            else {
+
+                #if DEBUG
+                    let picUrl = backgroundImageURL.stringByReplacingOccurrencesOfString("https", withString: "http", options: NSStringCompareOptions.LiteralSearch, range: nil)
+                #else
+                    let picUrl = backgroundImageUrl
+                #endif
+                
+                backgroundImageView.hnk_setImageFromURL(NSURL(string: picUrl)!)
+            }
+        }
+    }
+    
+    // MARK: - Loading
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        backgroundImageView.contentMode = .ScaleAspectFill
+    }
+}
