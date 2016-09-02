72e3456e8795d4d1aedf2aa927fd5d70f57f030d
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-09-01 19:55:45 -0500

    working on Handoff for Events

--- OpenStack Summit/OpenStack Summit/Define.swift
+++ OpenStack Summit/OpenStack Summit/Define.swift
@@ -16,6 +16,7 @@ public let AppVersion = NSBundle.mainBundle().infoDictionary!["CFBundleShortVers
 /// Build of the app.
 public let AppBuild = Int(NSBundle.mainBundle().infoDictionary!["CFBundleVersion"] as! String)!
 
+#if os(iOS)
 /// The app's environment.
 let AppEnvironment: Environment = {
     
@@ -26,6 +27,13 @@ let AppEnvironment: Environment = {
     
     return environment
 }()
+#elseif os(watchOS)
+    #if Production
+    let AppEnvironment = Environment.Production
+    #elseif Staging
+    let AppEnvironment = Environment.Staging
+    #endif
+#endif
 
 /// The app's configuration.
 let AppConfiguration: Configuration = {
