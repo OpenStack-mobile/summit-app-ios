264d266889b75678896466936341e4abc3a48dd9
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-09-06 15:56:40 -0500

    [tvOS] working on EventDetailVC

--- OpenStack Summit/OpenStackSummitTV/EventDetailViewController.swift
+++ OpenStack Summit/OpenStackSummitTV/EventDetailViewController.swift
@@ -10,13 +10,19 @@ import Foundation
 import UIKit
 import CoreSummit
 
-final class EventDetailViewController: UITableViewController {
+final class EventDetailViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: - IB Outlets 
+    
+    @IBOutlet weak var tableView: UITableView!
     
     // MARK: - Properties
     
     var event: Identifier!
     
     private var eventDetail: EventDetail!
+    
+    private var data = [Detail]()
     
     // MARK: - Loading
     
@@ -37,8 +43,49 @@ final class EventDetailViewController: UITableViewController {
         
         self.eventDetail = EventDetail(realmEntity: realmEvent)
         
-        self.title = eventDetail.name
+        self.data = [Detail]()
         
+        data.append(.name(eventDetail.name))
+        
+        self.tableView.reloadData()
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return data.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let detail = self.data[indexPath.row]
+        
+        switch detail {
+            
+        case let .name(name):
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier("EventNameCell", forIndexPath: indexPath)
+            
+            cell.textLabel!.text = name
+            
+            return cell
+        }
+    }
+}
+
+// MARK: - Supporting Types
+
+private extension EventDetailViewController {
+    
+    enum Detail {
+        
+        case name(String)
         
     }
 }
