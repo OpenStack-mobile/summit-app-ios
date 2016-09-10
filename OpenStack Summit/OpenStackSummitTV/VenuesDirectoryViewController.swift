f25c210bfa9e2e58fb568c5bb1745709c2d59969
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-09-10 10:59:12 -0500

    [tvOS] working on Venues

---
+++ OpenStack Summit/OpenStackSummitTV/VenuesDirectoryViewController.swift
@@ -0,0 +1,11 @@
+//
+//  VenuesDirectoryViewController.swift
+//  OpenStack Summit
+//
+//  Created by Alsey Coleman Miller on 9/10/16.
+//  Copyright © 2016 OpenStack. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
