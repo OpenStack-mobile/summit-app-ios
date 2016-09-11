32d05aa4c64be4ac9f9fcbddbd1ac69080f6e901
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-09-10 23:38:25 -0500

    [tvOS] added metadata for YouTube video player

--- OpenStack Summit/OpenStackSummitTV/PlayVideo.swift
+++ OpenStack Summit/OpenStackSummitTV/PlayVideo.swift
@@ -19,20 +19,55 @@ extension UIViewController {
             
             guard let controller = self else { return }
             
-            guard error == nil else {
-                
-                controller.showErrorAlert(error!.localizedDescription)
-                return
-            }
+            guard error == nil
+                else { controller.showErrorAlert(error!.localizedDescription); return }
             
             guard let streamURL = youtubeVideo!.streamURLs[XCDYouTubeVideoQuality.HD720.rawValue as NSNumber]
                 ?? youtubeVideo!.streamURLs[XCDYouTubeVideoQuality.Medium360.rawValue as NSNumber]
                 ?? youtubeVideo!.streamURLs[XCDYouTubeVideoQuality.Small240.rawValue as NSNumber]
                 else { controller.showErrorAlert("YouTube API Error"); return }
             
+            let asset = AVAsset(URL: streamURL)
+            let playerItem = AVPlayerItem(asset: asset)
+            playerItem.externalMetadata = {
+                
+                var metadata = [AVMutableMetadataItem]()
+               
+                let titleItem = AVMutableMetadataItem()
+                titleItem.identifier = AVMetadataCommonIdentifierTitle
+                titleItem.value = video.name
+                titleItem.extendedLanguageTag = "und"
+                metadata.append(titleItem)
+                
+                if let data = video.descriptionText?.dataUsingEncoding(NSUTF8StringEncoding),
+                    let attributedString = try? NSAttributedString(data: data, options: [NSDocumentTypeDocumentAttribute:NSHTMLTextDocumentType,NSCharacterEncodingDocumentAttribute:NSUTF8StringEncoding], documentAttributes: nil) {
+                    
+                    let descriptionItem = AVMutableMetadataItem()
+                    descriptionItem.identifier = AVMetadataCommonIdentifierDescription
+                    descriptionItem.value = attributedString.string
+                    descriptionItem.extendedLanguageTag = "und"
+                    metadata.append(descriptionItem)
+                }
+                
+                if let thumbnailURL = NSURL(youtubeThumbnail: video.youtube),
+                    let data = NSData(contentsOfURL: thumbnailURL) {
+                    
+                    let item = AVMutableMetadataItem()
+                    item.identifier = AVMetadataCommonIdentifierArtwork
+                    item.dataType = kCMMetadataBaseDataType_PNG as String
+                    item.value = data
+                    item.extendedLanguageTag = "und"
+                    metadata.append(item)
+                }
+                
+                return metadata
+            }()
+            
             let playerViewController = AVPlayerViewController()
-            playerViewController.player = AVPlayer(URL: streamURL)
-            controller.presentViewController(playerViewController, animated: true) { playerViewController.player?.play() }
+            let player = AVPlayer(playerItem: playerItem)
+            playerViewController.player = player
+            
+            controller.presentViewController(playerViewController, animated: true) { player.play() }
         }
     }
 }
