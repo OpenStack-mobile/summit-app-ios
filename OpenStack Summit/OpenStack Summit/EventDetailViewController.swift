8f54e64108040aa459491b562fc69faf2cbbb196
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-09-29 13:07:45 -0500

    [iOS] [11816] can give feedback after event started, and if there is no feedback for that user

--- OpenStack Summit/OpenStack Summit/EventDetailViewController.swift
+++ OpenStack Summit/OpenStack Summit/EventDetailViewController.swift
@@ -161,9 +161,9 @@ final class EventDetailViewController: UITableViewController, ShowActivityIndica
             data.append(.video)
         }
         
-        // can give feedback after event ended, and if there is no feedback for that user
+        // can give feedback after event started, and if there is no feedback for that user
         if let attendee = Store.shared.authenticatedMember?.attendeeRole
-            where eventCache.end < Date() &&
+            where eventCache.start < Date() &&
             Store.shared.realm.objects(RealmFeedback).filter("event.id = %@ AND owner.id = %@", event, attendee.id).isEmpty {
             
             data.append(.feedback)
