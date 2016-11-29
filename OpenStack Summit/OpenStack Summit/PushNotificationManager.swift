1de8e0dd1ed40797d858f90f485fabf8672210bf
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-11-29 12:28:35 -0500

    [iOS] [12139] fixes for `SummitManager`

--- OpenStack Summit/OpenStack Summit/PushNotificationManager.swift
+++ OpenStack Summit/OpenStack Summit/PushNotificationManager.swift
@@ -17,38 +17,44 @@ public final class PushNotificationsManager {
         
         var channels = [String]()
         
-        if let summit = try! Store.shared.managedObjectContext.managedObjects(Summit).first {
+        // TODO: Observe summit manager and authenticated member
+        
+        let context = Store.shared.managedObjectContext
+        let summitID = SummitManager.shared.summit.value
+        guard let summit = try! SummitManagedObject.find(summitID, context: context)
+            else { return }
+        
+        channels.append("su_\(summit.identifier)")
+        
+        if let member = Store.shared.authenticatedMember {
             
-            channels.append("su_\(summit.identifier)")
+            channels.append("me_\(member.id)")
             
-            if let member = Store.shared.authenticatedMember {
-                
-                channels.append("me_\(member.id)")
-                
-                if member.attendeeRole != nil {
-                    channels.append("attendees")
-                }
-                
-                if member.speakerRole != nil {
-                    channels.append("speakers")
-                }
+            if member.attendeeRole != nil {
+                channels.append("attendees")
             }
             
-            PFInstallation.currentInstallation()!.channels = channels
-            PFInstallation.currentInstallation()!.saveEventually(completionBlock)
+            if member.speakerRole != nil {
+                channels.append("speakers")
+            }
         }
+        
+        PFInstallation.currentInstallation()!.channels = channels
+        PFInstallation.currentInstallation()!.saveEventually(completionBlock)
     }
     
     public static func unsubscribeFromPushChannels(completionBlock: (succeeded: Bool, error: NSError?) -> ()) {
         
         var channels = [String]()
         
-        if let summit = try! Store.shared.managedObjectContext.managedObjects(Summit).first {
-            
-            channels.append("su_\(summit.identifier)")
-            
-            PFInstallation.currentInstallation()!.channels = channels
-            PFInstallation.currentInstallation()!.saveEventually(completionBlock)
-        }
+        let context = Store.shared.managedObjectContext
+        let summitID = SummitManager.shared.summit.value
+        guard let summit = try! SummitManagedObject.find(summitID, context: context)
+            else { return }
+        
+        channels.append("su_\(summit.identifier)")
+        
+        PFInstallation.currentInstallation()!.channels = channels
+        PFInstallation.currentInstallation()!.saveEventually(completionBlock)
     }
 }
