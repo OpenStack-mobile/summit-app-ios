ffbd381add212f09a7585d61bb9ef521cb729992
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-01-03 11:42:38 -0500

    [CoreSummit] [11226] working on Teams API

--- OpenStack Summit/CoreSummit/TeamInvitationRequest.swift
+++ OpenStack Summit/CoreSummit/TeamInvitationRequest.swift
@@ -7,3 +7,40 @@
 //
 
 import SwiftFoundation
+import CoreData
+
+public extension Store {
+    
+    func accept(invitation identifier: Identifier, completion: (ErrorType?) -> ()) {
+        
+        let uri = "api/v1/members/me/team-invitations/\(identifier)"
+        
+        let url = environment.configuration.serverURL + uri
+        
+        let http = self.createHTTP(.OpenIDJSON)
+        
+        http.PUT(url) { (responseObject, error) in
+            
+            completion(error)
+        }
+    }
+    
+    func decline(invitation identifier: Identifier, completion: (ErrorType?) -> ()) {
+        
+        let uri = "api/v1/members/me/team-invitations/\(identifier)"
+        
+        let url = environment.configuration.serverURL + uri
+        
+        let http = self.createHTTP(.OpenIDJSON)
+        
+        http.DELETE(url) { (responseObject, error) in
+            
+            completion(error)
+        }
+    }
+    
+    func invitations(completion: (ErrorValue<Page<TeamInvitation>>) -> ()) {
+        
+        
+    }
+}
