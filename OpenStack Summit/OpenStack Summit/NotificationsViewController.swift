cf03d2f8f4cdfd60e14ef3e1770e80fdfe174e70
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-01-27 14:38:29 -0500

    [iOS] [12369] working on Notifications

--- OpenStack Summit/OpenStack Summit/NotificationsViewController.swift
+++ OpenStack Summit/OpenStack Summit/NotificationsViewController.swift
@@ -12,9 +12,131 @@ import CoreData
 import CoreSummit
 import XLPagerTabStrip
 
-final class NotificationsViewController: UITableViewController, IndicatorInfoProvider {
+final class NotificationsViewController: UITableViewController, NSFetchedResultsControllerDelegate, IndicatorInfoProvider {
+    
+    // MARK: - Properties
+    
+    private var fetchedResultsController: NSFetchedResultsController!
+    
+    private lazy var dateFormatter: NSDateFormatter = {
+       
+        let dateFormatter = NSDateFormatter()
+        
+        dateFormatter.dateStyle = .ShortStyle
+        
+        dateFormatter.timeStyle = .ShortStyle
+        
+        return dateFormatter
+    }()
     
     // MARK: - Loading
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.estimatedRowHeight = 44
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        
+        configureView()
+    }
+    
+    // MARK: - Private Methods
+    
+    private func configureView() {
+        
+        let sort = [NSSortDescriptor(key: "id", ascending: false)]
+        
+        self.fetchedResultsController = NSFetchedResultsController(Notification.self,
+                                                                   delegate: self,
+                                                                   predicate: nil,
+                                                                   sortDescriptors: sort,
+                                                                   sectionNameKeyPath: nil,
+                                                                   context: Store.shared.managedObjectContext)
+        
+        try! self.fetchedResultsController.performFetch()
+        
+        self.tableView.reloadData()
+    }
+    
+    private subscript (indexPath: NSIndexPath) -> Notification {
+        
+        let managedObject = self.fetchedResultsController.objectAtIndexPath(indexPath) as! Notification.ManagedObject
+        
+        return Notification(managedObject: managedObject)
+    }
+    
+    private func configure(cell cell: UITableViewCell, at indexPath: NSIndexPath) {
+        
+        let notification = self[indexPath]
+        
+        cell.textLabel!.text = notification.body
+        
+        cell.detailTextLabel!.text = self.dateFormatter.stringFromDate(notification.created.toFoundation())
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return self.fetchedResultsController.fetchedObjects?.count ?? 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier(R.reuseIdentifier.notificationCell)!
+        
+        configure(cell: cell, at: indexPath)
+        
+        return cell
+    }
+    
+    // MARK: - NSFetchedResultsControllerDelegate
+    
+    func controllerWillChangeContent(controller: NSFetchedResultsController) {
+        
+        self.tableView.beginUpdates()
+    }
+    
+    func controllerDidChangeContent(controller: NSFetchedResultsController) {
+        
+        self.tableView.endUpdates()
+    }
+    
+    func controller(controller: NSFetchedResultsController, didChangeObject anObject: AnyObject, atIndexPath indexPath: NSIndexPath?, forChangeType type: NSFetchedResultsChangeType, newIndexPath: NSIndexPath?) {
+        
+        switch type {
+        case .Insert:
+            
+            if let insertIndexPath = newIndexPath {
+                self.tableView.insertRowsAtIndexPaths([insertIndexPath], withRowAnimation: .Fade)
+            }
+        case .Delete:
+            
+            if let deleteIndexPath = indexPath {
+                self.tableView.deleteRowsAtIndexPaths([deleteIndexPath], withRowAnimation: .Fade)
+            }
+        case .Update:
+            if let updateIndexPath = indexPath,
+                let cell = self.tableView.cellForRowAtIndexPath(updateIndexPath) {
+                
+                self.configure(cell: cell, at: updateIndexPath)
+            }
+        case .Move:
+            
+            if let deleteIndexPath = indexPath {
+                self.tableView.deleteRowsAtIndexPaths([deleteIndexPath], withRowAnimation: .Fade)
+            }
+            
+            if let insertIndexPath = newIndexPath {
+                self.tableView.insertRowsAtIndexPaths([insertIndexPath], withRowAnimation: .Fade)
+            }
+        }
+    }
     
     // MARK: - IndicatorInfoProvider
     
