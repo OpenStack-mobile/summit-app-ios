ffc7b4b39bfa91a4c61a117b3a0732c7a46d443f
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-02-10 00:37:51 -0500

    updated buddy build script

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -2,6 +2,19 @@
 cp -rf ${BUDDYBUILD_SECURE_FILES}/Production.swift ./OpenStack\ Summit/CoreSummit/
 cp -rf ${BUDDYBUILD_SECURE_FILES}/Staging.swift ./OpenStack\ Summit/CoreSummit/
 
-# Extract Vendor frameworks
-mkdir Vendor
-unzip -o ${BUDDYBUILD_SECURE_FILES}/Vendor.zip
+# Copy iOS environment source files
+cp -rf ${BUDDYBUILD_SECURE_FILES}/AppConsumerKey.swift ./OpenStack\ Summit/OpenStack\ Summit/
+cp -rf ${BUDDYBUILD_SECURE_FILES}/Beta.entitlements ./OpenStack\ Summit/
+mkdir ./OpenStack\ Summit/GoogleServices
+mkdir ./OpenStack\ Summit/GoogleServices/Beta
+cp -rf ${BUDDYBUILD_SECURE_FILES}/GoogleService-Info.plist ./OpenStack\ Summit/GoogleServices/Beta/
+
+# Install Vendor frameworks
+unzip -o ${BUDDYBUILD_SECURE_FILES}/VendorFabric.zip
+unzip -o ${BUDDYBUILD_SECURE_FILES}/VendorFirebase.zip
+
+# Download and install Google Maps
+
+
+# Generate R.swift
+./OpenStack\ Summit/rswift -p ./OpenStack\ Summit/OpenStack Summit.xcodeproj -t OpenStackSummit --bundleIdentifier org.openstack.OpenStack-Summit ./OpenStack\ Summit/
