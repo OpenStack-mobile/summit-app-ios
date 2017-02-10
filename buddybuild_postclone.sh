b0efe00e07f881bb9e26a8e03aee357e8ef98824
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-02-10 11:11:03 -0500

    Fixed BuddyBuild script

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -17,4 +17,4 @@ unzip -o ${BUDDYBUILD_SECURE_FILES}/VendorFirebase.zip
 # Download and install Google Maps
 wget https://www.gstatic.com/cpdc/aa3052925ceeea2d-GoogleMaps-1.13.2.tar.gz
 tar -zxvf aa3052925ceeea2d-GoogleMaps-1.13.2.tar.gz Frameworks/GoogleMaps.framework
-cp -rf ./Frameworks ./Vendor
+cp -rf ./Frameworks/* ./Vendor
