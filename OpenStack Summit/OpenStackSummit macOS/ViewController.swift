d4dba3fd83c958b121643778abcb6abe514cf89b
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-02-18 13:20:40 -0500

    [macOS] Added OS X target

---
+++ OpenStack Summit/OpenStackSummit macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Summit
+//
+//  Created by Alsey Coleman Miller on 2/18/17.
+//  Copyright © 2017 OpenStack. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
