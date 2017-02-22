e033cdecf6dcefde797b79f5a0679fa9927ff51c
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-02-21 21:45:01 -0500

    working on OS X app

--- OpenStack Summit/OpenStackSummit macOS/SearchViewController.swift
+++ OpenStack Summit/OpenStackSummit macOS/SearchViewController.swift
@@ -11,9 +11,36 @@ import AppKit
 import CoreData
 import CoreSummit
 
-final class SearchViewController: NSViewController, NSTableViewDelegate, NSTableViewDataSource {
+final class SearchViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
     
     // MARK: - IB Outlets
     
     @IBOutlet private(set) weak var tableView: NSTableView!
+    
+    // MARK: - Properties
+    
+    
+    
+    // MARK: - Loading
+    
+    
+    
+    // MARK: - Actions
+    
+    
+    
+    // MARK: - UITableViewDataSource
+    
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        
+        return self.fetchedResultsController?.fetchedObjects?.count ?? 0
+    }
+    
+    // MARK: - NSTableViewDelegate
+    
+    
 }
+
+// MARK: - Supporting Types
+
+private extension SearchViewController
