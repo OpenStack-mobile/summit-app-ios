ae70f6c0284ca28c1c541261dda890073992db62
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-05-05 08:01:50 -0500

    [CoreSummit] [12879] Implement parsing of topics with and without platform prefix (#103)
    Currently topics are sent with platform prefix (/topics/ios_everyone)
    If install is stuck with a record containing the old way we handled
    topics (/topics/everyone), retrieving notification from core data will
    fail. This will crash the app when entering inbox section.
    Implement patch to parse topic with and without platform prefix.

---
+++ OpenStack Summit/CoreSummitTests/ModelTests.swift
@@ -0,0 +1,33 @@
+//
+//  ModelTests.swift
+//  OpenStack Summit
+//
+//  Created by Alsey Coleman Miller on 5/5/17.
+//  Copyright © 2017 OpenStack. All rights reserved.
+//
+
+import XCTest
+import SwiftFoundation
+@testable import CoreSummit
+
+final class ModelTests: XCTestCase {
+
+    func testNotificationTopicParse() {
+        
+        // Valid strings
+        XCTAssert(Notification.Topic(rawValue: "/topics/ios_everyone") == Notification.Topic.everyone)
+        XCTAssert(Notification.Topic(rawValue: "/topics/everyone") == Notification.Topic.everyone)
+        XCTAssert(Notification.Topic(rawValue: "/topics/ios_member_1") == Notification.Topic.member(1))
+        XCTAssert(Notification.Topic(rawValue: "/topics/member_1") == Notification.Topic.member(1))
+        
+        // Invalid Strings
+        XCTAssert(Notification.Topic(rawValue: "/topics/ios_") == nil)
+        XCTAssert(Notification.Topic(rawValue: "/topics/") == nil)
+        XCTAssert(Notification.Topic(rawValue: "/topics") == nil)
+        XCTAssert(Notification.Topic(rawValue: "/topics/fakeTopic") == nil)
+        XCTAssert(Notification.Topic(rawValue: "/topics/ios_fakeTopic") == nil)
+        XCTAssert(Notification.Topic(rawValue: "/topics/ios_member_") == nil)
+        XCTAssert(Notification.Topic(rawValue: "/topics/ios_member_a") == nil)
+        XCTAssert(Notification.Topic(rawValue: "/topics/ios_member") == nil)
+    }
+}
