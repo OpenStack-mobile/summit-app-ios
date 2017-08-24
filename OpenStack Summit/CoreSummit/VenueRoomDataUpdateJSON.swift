3d262b3cf8a19b07be0578414295e02733ca7f98
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2017-08-24 19:06:17 -0300

    [CoreSummit] [13204] Makes VenueRoom floor optional.

--- OpenStack Summit/CoreSummit/VenueRoomDataUpdateJSON.swift
+++ OpenStack Summit/CoreSummit/VenueRoomDataUpdateJSON.swift
@@ -22,15 +22,24 @@ extension VenueRoom.DataUpdate: JSONDecodable {
             let identifier = JSONObject[LocationJSONKey.id.rawValue]?.integerValue,
             let name = JSONObject[LocationJSONKey.name.rawValue]?.rawValue as? String,
             let venueIdentifier = JSONObject[JSONKey.venue_id.rawValue]?.integerValue,
-            let floorJSON = JSONObject[JSONKey.floor.rawValue],
-            let floor = VenueFloor(json: floorJSON),
             classNameString == VenueRoom.JSONClassName
             else { return nil }
         
         self.identifier = identifier
         self.name = name
         self.venue = venueIdentifier
-        self.floor = floor
+        
+        if let floorJSON = JSONObject[JSONKey.floor.rawValue] {
+            
+            guard let floor = VenueFloor(json: floorJSON)
+                else { return nil }
+            
+            self.floor = floor
+            
+        } else {
+            
+            self.floor = nil
+        }
         
         // optional
         self.descriptionText = JSONObject[LocationJSONKey.description.rawValue]?.rawValue as? String
