5ff7442f09fc62c00f52805b8f4d3f1a192b6111
Gabriel Cutrini <gabriel.cutrini@gmail.com>
2018-02-16 16:42:15 -0300

    [iOS] [11887] Adds schedule url deep linking.

--- OpenStack Summit/OpenStack Summit/AppDelegate.swift
+++ OpenStack Summit/OpenStack Summit/AppDelegate.swift
@@ -194,7 +194,8 @@ final class AppDelegate: UIResponder, UIApplicationDelegate, SummitActivityHandl
         
         // HACK: async is needed in case app was not already opened
         DispatchQueue.main.async {
-            let _ = self.openScheme(url: url)
+            
+            let _ = self.openWeb(url: url)
         }
         
         return false
